694b420a5bac2bbbac2cd8ff3c105a3f37088fd4
A. Gabiola <agustingabiola@hotmail.com>
2016-05-23 20:15:47 -0300

    Agregado version de expiracion

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
+
+    if (QDateTime::currentDateTime().date().year() >= 2017) {
+        QMessageBox::information(0, "Contactarse con el programador!"
+                             , "La version de prueba ha expirado!! \n"
+                             , QMessageBox::Ok);
+        return 0;
+    }
+
     MainWindow w;
     w.show ();
     return a.exec ();
